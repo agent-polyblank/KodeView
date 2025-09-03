c9b40f9fa9f0c80ea39a64726ff3409d12134b86
agent-polyblank <kingofsams2020@gmail.com>
2025-09-03 16:06:57 +0100

    add line number toggle to example app

--- androidExample/src/main/java/dev/snipme/androidexample/LineNumberSwitcher.kt
+++ androidExample/src/main/java/dev/snipme/androidexample/LineNumberSwitcher.kt
@@ -9,16 +9,20 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.FormatAlignLeft
 import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.outlined.FormatAlignLeft
+import androidx.compose.material.icons.outlined.FormatListNumbered
 import androidx.compose.material.icons.outlined.LightMode
+import androidx.compose.material.icons.outlined.Reorder
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ThemeSwitcher(
-    isDarkMode: Boolean,
+fun LineNumberSwitcher(
+    lineNumbersEnabled: Boolean,
     modifier: Modifier = Modifier,
     onChange: (Boolean) -> Unit,
 ) {
@@ -29,11 +33,11 @@ fun ThemeSwitcher(
             verticalAlignment = Alignment.CenterVertically,
         ) {
             Spacer(Modifier.width(8.dp))
-            Icon(Icons.Outlined.LightMode, contentDescription = "Light mode")
+            Icon(Icons.Outlined.FormatListNumbered, contentDescription = "Line numbers enabled")
             Spacer(Modifier.width(16.dp))
-            Switch(checked = isDarkMode, onCheckedChange = onChange)
+            Switch(checked = lineNumbersEnabled, onCheckedChange = onChange)
             Spacer(Modifier.width(16.dp))
-            Icon(Icons.Default.DarkMode, contentDescription = "Dark mode")
+            Icon(Icons.Outlined.Reorder, contentDescription = "Dark mode")
             Spacer(Modifier.width(8.dp))
         }
     }
